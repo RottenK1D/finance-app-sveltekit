902877f8268e2b7fa698c7bc8197e4dce67cd3da
nuker <rinards.sondors@gmail.com>
2022-10-03 20:42:49 +0100

    add head tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Next Generation Finance</title>
+	<meta />Modern finance technologies and services , build with Sveltekit and Tailwindcss
+</svelte:head>
+
+<meta />
 <slot />
